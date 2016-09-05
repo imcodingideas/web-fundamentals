fe2d60cb9c8e50c8ef009d2bef8c6894d9e9e186
Joseph Chambers <joseph@michael-chambers.com>
2016-09-05 18:04:24 -0500

    GET and POST in Sinatra

--- knowing_sinatra/app/controllers/index.rb
+++ knowing_sinatra/app/controllers/index.rb
@@ -1,5 +1,26 @@
 get '/' do
-  # La siguiente linea hace render de la vista 
-  # que esta en app/views/index.erb
+  puts "This is a GET to (/)"
+  puts "This is to get the value of Params: #{params.inspect}"
   erb :index
+end
+
+get '/cool_url' do
+
+  @greeting = params[:greeting]
+  @name = params[:name]
+
+  puts "This is a GET to /cool_url"
+  puts "This is to get the value of Params: #{params.inspect}"
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+
+  @greeting = params[:greeting]
+  @name = params[:name]
+
+  puts "This is a POST to /cool_url"
+  puts "This is to get the value of Params: #{params.inspect}"
+
+  erb :post_cool_url
 end
