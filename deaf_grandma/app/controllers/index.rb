0f75c8a018a173f5adbfc513c5b4a19a56251256
Joseph Chambers <joseph@michael-chambers.com>
2016-09-05 18:33:23 -0500

    deaf grandma

--- deaf_grandma/app/controllers/index.rb
+++ deaf_grandma/app/controllers/index.rb
@@ -2,11 +2,18 @@ get '/' do
   @abuelita = params[:abuelita]
   
   erb :index
-  # Visita app/views/index.erb
 end
-
 
 post '/abuelita' do
   user_input = params[:user_input]
-end
+  user_input.to_s
 
+  answer = true
+  user_input.each_char { |x| answer = false if x != x.upcase }
+  if answer == true
+    (user_input == 'BYE') ? 'TAKE CARE SON, SEE YOU LATER' : 'NO, NOT SINCE 1983'
+  else
+    'HUH? I CANT HEAR YOU SON!'
+  end
+
+end
