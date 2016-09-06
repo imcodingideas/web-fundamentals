4fbfcf9d33017a91b9c7c81cf8be79f1a4f7c877
Joseph Chambers <joseph@michael-chambers.com>
2016-09-06 13:29:09 -0500

    Used jquery to make the tabs to work and content to display

--- tabs_skeleton/tabs.js
+++ tabs_skeleton/tabs.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
-  // Escribe tu código aquí
+    $('#tab1').css('display','block');
+    $(".tabs [href='#tab1']").click(function(){
+        $('#tab1').css('display','block');
+        $('#tab2').css('display','none');
+        $('#tab3').css('display','none');
+        $('li:nth-child(1)').addClass('active');
+        $('li:nth-child(2)').removeClass('active');
+        $('li:nth-child(3)').removeClass('active');
+    });
+    $(".tabs [href='#tab2']").click(function(){
+        $('#tab2').css('display','block');
+        $('#tab1').css('display','none');
+        $('#tab3').css('display','none');
+        $('li:nth-child(2)').addClass('active');
+        $('li:nth-child(1)').removeClass('active');
+        $('li:nth-child(3)').removeClass('active');
+    });
+    $(".tabs [href='#tab3']").click(function(){
+        $('#tab3').css('display','block');
+        $('#tab2').css('display','none');
+        $('#tab1').css('display','none');
+        $('li:nth-child(3)').addClass('active');
+        $('li:nth-child(2)').removeClass('active');
+        $('li:nth-child(1)').removeClass('active');
+    });
 });
